fe6aa3eb80fcb17f8379db741c480cb2c402d1cc
Gabriel Sandu <gabrrrielll@gmail.com>
2025-10-15 12:12:47 +0300

    Simplified XPath to search directly in card-1 class for property links

--- includes/constants.php
+++ includes/constants.php
@@ -23,7 +23,7 @@ if (!defined('ABSPATH')) {
 
 // Define the scraper configuration
 define('RES_SCRAPER_CONFIG', array(
-    'property_list_urls_xpath' => '//div[contains(@class, "main-cards")]//div[contains(@class, "card-1")]//a[@href]',
+    'property_list_urls_xpath' => '//div[contains(@class, "card-1")]//a[@href]',
     'property_data' => array(
         'title_xpath' => '//h1[@data-test-id="ad-title"]/span[1]',
         'content_xpath' => '//pre[@data-test-id="ad-description"]',
